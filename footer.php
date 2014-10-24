7b43193e1198be233f28fb339de66b310dc8b350
Tuater Vue <tuater@gmail.com>
2014-10-24 12:48:46 -0500

    made a few adjustments in mobile view

--- footer.php
+++ footer.php
@@ -109,7 +109,7 @@
         
         <div class="copyright">
         	<div>&copy;Copyright 2013. All rights reserved. College of Agricultural and Life Sciences - University of Wisconsin-Madison.</div>
-	        <img src="<?php echo get_template_directory_uri(); ?>/images/uwcrest_footer.png" alt="University of Wisconsin Madison" align="center" />
+	        <img crest src="<?php echo get_template_directory_uri(); ?>/images/uwcrest_footer.png" style="height:63px; width:40px;"alt="University of Wisconsin Madison" align="center" />
         </div>
 			<?php
 				/* A sidebar in the footer? Yep. You can can customize
